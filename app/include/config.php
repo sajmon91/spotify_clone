cee51c753cefec0770e0fb581e6924114a4d65a6
sajmon91 <stefan.sajmon@gmail.com>
2022-06-21 12:42:21 +0200

    Update config.php

--- app/include/config.php
+++ app/include/config.php
@@ -2,13 +2,18 @@
 
 
 // DB params
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'spotify_clone');
-
-
-
+if($_SERVER['SERVER_NAME'] == "localhost")
+{
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'spotify_clone');
+}else{
+    define('DB_HOST', 'remotemysql.com');
+    define('DB_USER', '6jQAwTiyjt');
+    define('DB_PASS', 'IiaNcZv0Hc');
+    define('DB_NAME', '6jQAwTiyjt');
+}
 
 
 
